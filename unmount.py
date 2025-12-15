2645144a309f9721351537a681c27256216be14a
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-15 21:20:22 +0000

    Add files via upload

--- unmount.py
+++ unmount.py
@@ -5,14 +5,18 @@ ff=f1.read()
 f1.close()
 index=0;
 fff=ff.split("|")
-fff=fff[1:]
 names=""
 for f in fff:
+    
     if index & 1 == 0:
+      
+      f=names.replace("|","")
       names=f
-      names.replace("|","")
+      print(f)
+      print(names)
     else:
       f1=open(names,"w")
+      f=f.replace("\xff","|")
       f1.write(f)
       f1.close()
     index=index+1
