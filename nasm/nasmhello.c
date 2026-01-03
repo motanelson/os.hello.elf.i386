e5c891965de9aedc8e0b88ea23e1261fc044d492
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-03 22:31:18 +0000

    Add files via upload

---
+++ nasm/nasmhello.c
@@ -0,0 +1,11 @@
+//nasm -f elf32 nasmhello.asm -o hello.o
+//gcc -m32 -nostdlib -fno-pie -no-pie hello.o nasmhello.c -o hello
+extern void putss (char *c);
+extern void exitss (int c);
+
+int main(){
+    char *c="hello................................\n";
+    putss(c);
+    exitss(0);
+    return 0;
+}
